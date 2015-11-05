bd2f247d0dca863868d64a156b02932c4de135bf
jamesyothers <jamesjsdev@gmail.com>
2015-11-04 18:27:48 -0800

    display random number on button click

--- app/src/main/java/com/integrityinovations/randomfact/randomFactActivity.java
+++ app/src/main/java/com/integrityinovations/randomfact/randomFactActivity.java
@@ -1,7 +1,13 @@
 package com.integrityinovations.randomfact;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class randomFactActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class randomFactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_fact);
+
+        // declare our view variables and assign them the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the button was clicked, so update the fact label with a new fact
+                String fact;
+                // randomly select a fact
+                // construct a new random number generator
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+                // update the label with our dynamic fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+
     }
 }
